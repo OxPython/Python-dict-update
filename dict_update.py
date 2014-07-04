9cdfa4c361ce5e5b082b3e87a0a59a26846547fb
Emerson <root@emerson-N53SN>
2014-07-04 06:05:50 -0500

    How can I merge (union) two Python dictionaries?

--- dict_update.py
+++ dict_update.py
@@ -5,20 +5,21 @@ Created on Jul 2, 2014
 
 How can I merge (union) two Python dictionaries?
 
-¿Cómo puedo combinar (unión) dos diccionarios de Python?
+¿Cómo puedo combinar (unión) dos diccionarios en Python?
 
-The method update() adds dictionary dict2's key-values pairs 
-in to dict. This function does not return anything.
+The method update() adds dictionary d2's key-values pairs 
+in to d. This function does not return anything.
 '''
 
 #dict with two values and keys
 d = {'Name': 'Emerson', 'Age': 27}
+print (d)
 
 #dict with one value and key
 d2 = {'Sex': 'Male' }
+print (d2)
 
 #Merge in one dict
 d.update(d2)
-
 print (d)
 
